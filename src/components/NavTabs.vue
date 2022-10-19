28e9e1b180c65c99f3eeaff7b5f78478b00cf665
Julie Chou <chouyuan424@gmail.com>
2022-10-19 14:11:57 -0400

    feat: add NavTabs

---
+++ src/components/NavTabs.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="nav nav-tabs mb-4">
+    <li class="nav-item">
+      <router-link to="/restaurants" class="nav-link"> 
+       Home 
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/restaurants/feeds" class="nav-link">
+        Latest Activity
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/restaurants/top" class="nav-link"> 
+        TOP 10 Restaurants
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/users/top" class="nav-link">
+        Top User
+      </router-link>
+    </li>
+  </ul>
+</template>
