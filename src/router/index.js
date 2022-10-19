8870cb7e32f019097fc458a5b384baff8f5e7608
Julie Chou <chouyuan424@gmail.com>
2022-10-19 13:15:01 -0400

    feat: add page components in NavTab(raw content only) and setup their routers

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import SignIn from '../views/SignIn.vue'
 import NotFound from '../views/NotFound.vue'
+import Restaurants from '../views/Restaurants.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+  path: '/',
+  name: 'root',
+  redirect: '/restaurants'
+  },
   {
     path: '/signin',
     name: 'sign-in',
@@ -14,6 +20,26 @@ const routes = [
     path: '/signup',
     name:'sign-up',
     component: () => import('../views/SignUp.vue')
+  },
+  {
+    path: '/restaurants',
+    name:'restaurants',
+    component: Restaurants
+  },
+  {
+    path:'/restaurants/feeds',
+    name:'restaurants-feeds',
+    component: () => import('../views/RestaurantsFeeds.vue')
+  },
+  {
+    path:'/restaurants/top',
+    name:'restaurants-tops',
+    component: () => import('../views/RestaurantsTop.vue')
+  },  
+  {
+    path:'/users/top',
+    name:'users-top',
+    component: () => import('../views/UsersTop.vue')
   },
   {
     path: '*',
---
+++ src/views/Restaurants.vue
@@ -0,0 +1,6 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="container py-5">
+    <h1 class="mt-5">首頁 - 餐廳列表</h1>
+  </div>
+</template>
