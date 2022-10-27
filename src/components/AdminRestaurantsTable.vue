391659fa53b79a344e361fcdb1ab205dd258632a
Julie Chou <chouyuan424@gmail.com>
2022-10-27 15:13:29 -0400

    feat: modify "delete restaurant" btn in Admin page

--- src/components/AdminRestaurantsTable.vue
+++ src/components/AdminRestaurantsTable.vue
@@ -27,7 +27,13 @@
 
           <a href="#" class="btn btn-link">Edit</a>
 
-          <button type="button" class="btn btn-link">Delete</button>
+          <button 
+            type="button" 
+            class="btn btn-link" 
+            @click.stop.prevent="deleteRestaurant(restaurant.id)"
+          >
+            Delete
+          </button>
         </td>
       </tr>
     </tbody>
@@ -1004,6 +1010,9 @@ export default {
     fetchRestaurants() {
       this.restaurants = dummyData.restaurants;
     },
+    deleteRestaurant(restaurantId){
+      this.restaurants = this.restaurants.filter(restaurant => restaurant.id !== restaurantId)
+    }
   },
 };
 </script>
