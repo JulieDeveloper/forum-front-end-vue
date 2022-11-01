675db5feeadd376354b5ef749a15bf41e07196c9
Julie Chou <chouyuan424@gmail.com>
2022-11-01 12:11:32 -0400

    fix: fix wrong directions in admin restaurants table

--- src/components/AdminRestaurantsTable.vue
+++ src/components/AdminRestaurantsTable.vue
@@ -24,13 +24,22 @@
         </td>
         <td class="d-flex justify-content-between">
           <router-link 
-            :to=" { name: 'admin-restaurant-edit', params: {id: restaurant.id} }" 
-            class="btn btn-link"
-          >
+            :to="{
+                name: 'admin-restaurant', 
+                params: {id: restaurant.id}
+            }" 
+            class="btn btn-link">
             Show
           </router-link>
 
-          <a href="#" class="btn btn-link">Edit</a>
+          <router-link 
+            :to=" { 
+               name: 'admin-restaurant-edit', 
+               params: {id: restaurant.id} 
+            }"
+            class="btn btn-link">
+            Edit
+          </router-link>
 
           <button 
             type="button" 
